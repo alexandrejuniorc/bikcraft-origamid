69cd2be48880c21086b4b16ec976a9b50487a0e7
Alexandre Junior <alexandrejunior408@gmail.com>
2021-12-16 15:14:25 -0300

    adiciona ativação dos itens de compra

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 
-/*
-HEADER -  DEIXA O MENU ATIVO QUANDO O USUÁRIO ESTÁ NA PÁGINA */
-
+// ATIVR LINKS DO MENU
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link){
@@ -17,3 +15,17 @@ function ativarLink(link){
 }
 
 links.forEach(ativarLink);
+
+// ATIVAR ITENS DO ORÇAMENTO
+// CHECA OS ITENS DA URL PRA VER SE CHECA O ITEM OU NÃO.
+const parametros = new URLSearchParams (location.search); //new cria a classe, esse parametro serve para fazer buscas no link da página.
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro);
+    
+    if(elemento){
+        elemento.checked= true;
+    }
+   
+}
+parametros.forEach(ativarProduto);
